bb4136a45834532e443e9592e9778237c8ff8e82
jhargett1 <119984652+jhargett1@users.noreply.github.com>
2023-09-14 17:18:32 -0500

    Adding more emojis

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -19,6 +19,41 @@ const emojipedia = [
       name: "Rolling On The Floor, Laughing",
       meaning:
         "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    }, 
+    {
+      id: 4,
+      emoji: "😀",
+      name: "Grinning Face",
+      meaning:
+        "The grinning face expresses joy, happiness and satisfaction and is a very common and popular emoji in digital communication. It is often used to simply say hello, to express joy or excitement about something, or to break up a short text."
+    },
+    {
+      id: 5, 
+      emoji: "😁",
+      name: "Grinning Face With Smiling Eyes",
+      meaning:
+        "This emoji shows a mischievous grin that represents lightheartedness and exuberance. It is often used to show that you are happy about an event, excited, or just a little embarrassed. In conversation, this emoji can be used to express positive feelings and enthusiasm, whether about personal achievements or joyful expectations."
+    },
+    {
+      id: 6,
+      emoji: "🥹",
+      name: "Face Holding Back Tears",
+      meaning:
+        "Shows a slightly smiling face with tears in its eyes, can express various emotions such as gratitude, emotion, or pride. At the same time, anger or rage can also be the reason why tears can hardly be held back. It is often used to express strong feelings in communication, and the emotional interpretation depends on the context."
+    },
+    {
+      id: 7,
+      emoji: "😂",
+      name: "Face With Tears of Joy",
+      meaning:
+        "Expresses that you can barely hold yourself on the chair with laughter. It symbolizes a situation where something is so incredibly funny that you are in tears and in a fit of laughter."
+    },
+    {
+      id: 8,
+      emoji: "😅",
+      name: "Smiling Face With Open Mouth & Cold Sweat",
+      meaning:
+        "A grinning face with drops of sweat is represented by the 😅 emoji. This is used to express relief that a tricky situation has been successfully handled or just gone well, such as an important meeting. It is also sometimes used to express nervousness or tension about upcoming difficult events."
     }
   ];
 
